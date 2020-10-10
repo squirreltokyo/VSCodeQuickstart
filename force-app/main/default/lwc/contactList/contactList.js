159ac68a735a4035f057df7cb33fad4050547adf
pleasetellme@163.com <pleasetellme@163.com>
2020-10-10 23:01:23 +0900

    Use Apex to Work with Data

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,15 @@
+import { LightningElement, wire } from 'lwc';
+import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+const COLUMNS = [
+    { label: 'FirstName', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'LastName', fieldName: LAST_NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+}
