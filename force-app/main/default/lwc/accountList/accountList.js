e2fe30371254fa91f5882e2a84c79378440c004c
pleasetellme@163.com <pleasetellme@163.com>
2020-10-12 12:52:09 +0900

    Handle Server Errors

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
@@ -12,4 +13,9 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getAccounts)
     accounts;
+    
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
 }
